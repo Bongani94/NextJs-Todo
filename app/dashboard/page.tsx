912802cbf3cb95002dec57bf62f0657bede05451
Bongani <bonganibaloyi94@gmail.com>
2025-08-04 05:35:14 +0200

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ const DashboardPage = () => {
   return (
     <div className="flex items-center flex-col justify-center w-full">
       <h1 className="text-4xl font-bold mt-52">Dashboard index page</h1>
-      <Link href="/dashboard/users">Users</Link>
+      <Link href="/dashboard/creation">Users</Link>
     </div>
   );
 };
